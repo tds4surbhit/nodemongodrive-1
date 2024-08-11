01bea583b01dc8a86023b97a6c8504241f7683ec
Surbhit Sinha <surbhitsinha@Surbhits-MacBook-Air.local>
2024-08-11 06:45:54 +0530

    adding latest changes

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const { connectToDb, getDb } = require("./db");
+const { ObjectId } = require("mongodb");
+
+// init app and middlewares
+const app = express();
+
+// db connect
+let db;
+connectToDb((err) => {
+  if (!err) {
+    app.listen(3000, () => {
+      console.log("APP , listining on port 3000");
+    });
+    db = getDb();
+    console.log("db", db);
+  }
+});
+
+// routes
+//find returns the cursor(points to whole collection of documents)//sheel behavious is It to iterate
+app.get("/books", async (req, res) => {
+  let Books = [];
+  db.collection("Books")
+    .find()
+    .sort({ author: 1 })
+    .forEach((book) => Books.push(book))
+    .then(() => {
+      res.status(200).json(Books);
+    })
+    .catch(() => {
+      res.status(500).json({ error: "Could not fetch the documents" });
+    });
+});
+
+// fetch single document
+app.get("/books/:id", (req, res) => {
+  db.collection("Books")
+    .findOne({ _id: ObjectId(req.params.id) })
+    .then((doc) => {
+      res.status(200).json(doc);
+    })
+    .catch((err) => {
+      res.status(500).json({ msg: "Couldn't get the document" });
+    });
+  req.params.id;
+});
